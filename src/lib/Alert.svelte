9f4042cc4e117af3f83841425a49807fe882abd4
Roger Espinosa <roger@umich.edu>
2022-12-12 16:10:04 -0500

    initial commit

---
+++ src/lib/Alert.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<svelte:options tag="my-alert" />
+
+<style>
+  div {
+    position: fixed;
+    bottom: 1rem;
+    left: 1rem;
+    width: auto;
+    padding: 2rem;
+    border: 4px solid #666;
+    background: black;
+    color: green;
+    border-radius: 1.5rem;
+  }
+</style>
+
+<div>THIS IS A THIRD TEST</div>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+// import './app.css'
+// import App from './App.svelte'
+
+// const app = new App({
+//   target: document.getElementById('app')
+// })
+
+// export default app
+
+export * from './lib/Counter.svelte';
+export * from './lib/Alert.svelte';
+export * from './lib/Header.svelte';
+export * from './lib/SearchForm.svelte';
